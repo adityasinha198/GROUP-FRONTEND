8028b62c2e6cbb765e6bcb9df8e742fc4af1fe15
adityasinha198 <adityasinha198@gmail.com>
2022-09-27 13:06:28 +0530

    task4 and task5

---
+++ login.js
@@ -0,0 +1,34 @@
+function login(event){
+    event.preventDefault();
+    
+    const emailid=event.target.emailid.value;
+    const password=event.target.password.value;
+    console.log(emailid)
+    const obj = {
+        emailid,
+        password
+    }
+   
+   axios.post("http://localhost:8000/login",obj)
+    .then(res => {
+        console.log(res.data.token)
+        const token = res.data.token
+        //const ispremiumuser = res.data.ispremiumuser
+        
+        localStorage.setItem("token",token)
+        //localStorage.setItem('ispremiumuser',ispremiumuser)
+        
+        alert("Logged in successfully")
+        //location.replace('/expense.html')
+    })
+    .catch(err=>{
+        if(err.response.status==404){
+            alert("User not found")
+        }
+        if(err.response.status==401){
+            alert("Unauthorised access")
+        }
+        
+    })
+}
+
