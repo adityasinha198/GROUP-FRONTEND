3df776caa615f852cdbe85a56f6393d82c005f3f
adityasinha198 <adityasinha198@gmail.com>
2022-09-27 12:33:46 +0530

    task2

---
+++ signup.js
@@ -0,0 +1,26 @@
+function onSubmit(event){
+    event.preventDefault();
+    const name=event.target.name.value;
+    const emailid=event.target.emailid.value;
+    const password=event.target.password.value;
+    const phonenumber = event.target.phonenumber.value
+    console.log(name)
+    const obj = {
+        name,
+        emailid,
+        phonenumber,
+        password
+    }
+   
+    axios.post("http://localhost:8000/signUp",obj)
+    .then(res=>{
+        console.log(res)
+    }).catch(err=>{
+        if(err.response.status==406){
+            alert("User created")
+        }
+        if(err.response.status==404){
+            alert("Emailid already exists")
+        }
+    })
+}
